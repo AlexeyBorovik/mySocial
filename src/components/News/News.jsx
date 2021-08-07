7dc1d54b3b5fe1872dc27dbb322cdb542f4e9deb
RobXela <78446731+RobXela@users.noreply.github.com>
2021-08-07 13:53:40 +0300

    News finished

--- src/components/News/News.jsx
+++ src/components/News/News.jsx
@@ -1,12 +1,22 @@
 import classes from './News.module.css'
+import Post from './Posts/Post';
 
 const News = () => {
-    return (<div className={classes.news}>
-        <div>Post1</div>
-        <div>Post2</div>
-        <div>Post3</div>
-        <div>Post4</div>
+    return (<div className={classes.news} >
+        <div>
+            News Area!!!
+            <br></br>
+            <textarea></textarea>
+            <br></br>
+            <button>Add New Post</button>
+        </div>
+
+        <div>
+            < Post text= "What`s up guys?" likes='2' dislikes='5'/>
+            < Post text= "It`s fist posting in my life!" likes='3' dislikes='3'/>
+            < Post text= "Coronavirus ended!!! We can leave the F*** masks!!!" likes='25' dislikes='0'/>
+        </div>
     </div>
-);
+    );
 }
 export default News;
--- src/components/News/Posts/Post.jsx
+++ src/components/News/Posts/Post.jsx
@@ -1,12 +1,25 @@
-import classes from './News.module.css'
+import classes from './Posts.module.css'
 
-const News = () => {
-    return (<div className={classes.news}>
-        <div>Post1</div>
-        <div>Post2</div>
-        <div>Post3</div>
-        <div>Post4</div>
+//let counter = ???? надо намутить счётчик 
+
+//надо классово разделить аву и картинку поста
+
+
+const Post = (props) => {
+    return (<div>
+        <div className={classes.post}>
+            <img id='ava' src="https://pbs.twimg.com/profile_images/1072566468796145665/H4n-JHm2.jpg" alt=" " />
+            {props.text}
+            <img id='post' src="Images/Posts Images/Ship.jpg" alt=" " />
+        </div>
+
+        <div>
+            <div> LIKE {props.likes}
+            </div>
+            <div> DISLIKE {props.dislikes}
+            </div>
+        </div>
     </div>
-);
+    );
 }
-export default News;
+export default Post;
