11b31a8885676ff3ab396de5a1c731a66fd39c45
RobXela <78446731+RobXela@users.noreply.github.com>
2021-08-15 03:00:04 +0300

    Messages&posts MAPing

--- src/components/Messages/Messages.jsx
+++ src/components/Messages/Messages.jsx
@@ -1,59 +1,58 @@
 import { NavLink } from 'react-router-dom'
 import classes from './Messages.module.css'
 
-const DialogueItem = (props) => {
-    let adress = '/messages/' + props.id
-    return (
-        <div className={classes.item}>
-            <NavLink to={adress}>{props.name}</NavLink>
-        </div>
-    )
-}
-
-const Message = (props) => {
-    return (
-       <div className={classes.message}>
-{props.text}
-       </div> 
-    )
-}
-
-
-let dialoguesData = [
-    {id: 1, name: 'Pasha'},
-    {id: 2, name: 'Ilya'},
-    {id: 3, name: 'Artyom'},
-    {id: 4, name: 'Lesha'},
-    {id: 5, name: 'Nataly'},
-    {id: 6, name: "Nastya"}
-]
-
-
-let messagesData = [
-    {id: 1, text: 'Hey, how is react?'},
-    {id: 2, text: 'Did you find a job?'},
-    {id: 3, text: 'You`re awesome'},
-    {id: 4, text: 'Hey Niko, it`s Roman'}
-]
-
 const Messages = (props) => {
+
+    const DialogueItem = (props) => {
+        let address = '/messages/' + props.name
+        return (
+            <div className={classes.item}>
+                <NavLink to={address}>{props.name}</NavLink>
+            </div>
+        )
+    }
+
+    const Message = (props) => {
+        return (
+            <div className={classes.message}>
+                {props.text}
+            </div>
+        )
+    }
+
+
+    let dialogues = [
+        { name: 'Pasha' },
+        { name: 'Ilya' },
+        { name: 'Artyom' },
+        { name: 'Lesha' },
+        { name: 'Nataly' },
+        { name: 'Nastya' },
+        { name: 'Roman'}
+    ]
+
+
+    let messages = [
+        { text: 'Hey, how is react?' },
+        { text: 'Did you find a job?' },
+        { text: 'You`re awesome' },
+        { text: 'Hey Niko, it`s Roman' },
+        { text: 'Why are you ignoring me?! You`re coding again?!' }
+    ]
+
+
+    let dialoguesElements = dialogues.map(dialogues => <DialogueItem name={dialogues.name} />);
+    let messageElements = messages.map(messages => <Message text={messages.text} />);
+
+
     return (
         <div className={classes.wrapper}>
             <div className={classes.dialoguesList}>
-                <DialogueItem id={dialoguesData[0].id} name={dialoguesData[0].name} />
-                <DialogueItem id={dialoguesData[1].id} name={dialoguesData[1].name} />
-                <DialogueItem id={dialoguesData[2].id} name={dialoguesData[2].name} />
-                <DialogueItem id={dialoguesData[3].id} name={dialoguesData[3].name} />
-                <DialogueItem id={dialoguesData[4].id} name={dialoguesData[4].name} />
-                <DialogueItem id={dialoguesData[5].id} name={dialoguesData[5].name} />
+                {dialoguesElements}
             </div>
 
-
             <div className={classes.dialogue}>
-                <Message text={messagesData[0].text}/>
-                <Message text={messagesData[1].text}/>
-                <Message text={messagesData[2].text}/>
-                <Message text={messagesData[3].text}/>
+                {messageElements}
             </div>
         </div>
     )
--- src/components/News/News.jsx
+++ src/components/News/News.jsx
@@ -2,29 +2,31 @@ import classes from './News.module.css'
 import Post from './Posts/Post';
 
 
-let newsData = [
-    {id: 1, likes: 2, dislikes: 5, text: "What`s up guys?"},
-    {id: 2, likes: 3, dislikes: 3, text: "It`s fist posting in my life!"},
-    {id: 3, likes: 25, dislikes: 0, text: "Coronavirus ended!!! We can leave the F*** masks!!!"}
-]
-
-
 const News = () => {
+
+    let post = [
+        { id: 1, likes: 2, dislikes: 5, text: "What`s up guys?" },
+        { id: 2, likes: 3, dislikes: 3, text: "It`s fist posting in my life!" },
+        { id: 3, likes: 25, dislikes: 0, text: "Coronavirus ended!!! We can leave the F*** masks!!!" },
+    ]
+
+    let postsElements = post.map(post => <Post text={post.text} likes={post.likes} dislikes={post.dislikes} />)
+
     return (<div className={classes.news} >
         <div>
             News Area!!!
             <br></br>
-            <textarea></textarea>
+            <textarea>НУ вводи уже!!!</textarea>
             <br></br>
             <button>Add New Post</button>
         </div>
 
         <div>
-            < Post text={newsData[0].text} likes={newsData[0].likes} dislikes={newsData[0].dislikes}/>
-            < Post text={newsData[1].text} likes={newsData[1].likes} dislikes={newsData[1].dislikes}/>
-            < Post text={newsData[2].text} likes={newsData[2].likes} dislikes={newsData[2].dislikes}/>
+            {postsElements}
         </div>
     </div>
     );
 }
+
+
 export default News;
