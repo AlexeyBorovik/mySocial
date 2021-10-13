9ebcdad5d44756ae22273349b560d74c831b0392
RobXela <78446731+RobXela@users.noreply.github.com>
2021-10-13 20:16:28 +0300

    install react-test-rerender

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import { unmountComponentAtNode } from 'react-dom';
+import MyReactApp from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+it('renders without crashing', () => {
+  const div = document.createElement('div')
+  render(<MyReactApp />, div);
+  unmountComponentAtNode(div)
 });
